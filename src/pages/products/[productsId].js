06453540b8ea2311e3f5e76b999a710b9ce9dfa3
saifur2233 <saifurnstuiit223344@gmail.com>
2023-11-26 12:28:34 +0600

    product detail and cart page design added

--- src/pages/products/[productsId].js
+++ src/pages/products/[productsId].js
@@ -1,44 +1,177 @@
 const ProductDetailPage = ({ product }) => {
   return (
-    <>
-      <div className="hero py-12">
-        <div className="hero-content flex-col lg:flex-row gap-12">
+    <div className="hero min-h-screen bg-base-200">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="lg:w-4/5 mx-auto flex flex-wrap">
           <img
+            alt="ecommerce"
+            className="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200"
             src={product?.image}
-            className="max-w-2xl rounded-lg shadow-2xl"
           />
-          <div>
-            <h1 className="text-3xl font-bold">{product?.productName}</h1>
-            <div className="gap-2 py-4">
-              <div className="badge badge-outline">{product?.category}</div>
-              <div className="badge badge-outline gap-1">
-                {product?.averageRating}
-                <div className="rating rating-sm">
-                  <input
-                    type="radio"
-                    name="rating-4"
-                    className="mask mask-star-2 bg-red-500"
-                  />
-                </div>
-              </div>
-              <div className="badge badge-outline">{product?.status}</div>
+          <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+            <h2 className="text-sm title-font text-gray-500 tracking-widest">
+              {product?.category}
+            </h2>
+            <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
+              {product?.productName}
+            </h1>
+            <div className="flex mb-4">
+              <span className="flex items-center">
+                <svg
+                  fill="currentColor"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  className="w-4 h-4 text-red-500"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                </svg>
+                <svg
+                  fill="currentColor"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  className="w-4 h-4 text-red-500"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                </svg>
+                <svg
+                  fill="currentColor"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  className="w-4 h-4 text-red-500"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                </svg>
+                <svg
+                  fill="currentColor"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  className="w-4 h-4 text-red-500"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                </svg>
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  className="w-4 h-4 text-red-500"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                </svg>
+                <span className="text-gray-600 ml-3">
+                  {product?.averageRating} Reviews
+                </span>
+              </span>
+              <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200">
+                <a className="text-gray-500">
+                  <svg
+                    fill="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    className="w-5 h-5"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                  </svg>
+                </a>
+                <a className="ml-2 text-gray-500">
+                  <svg
+                    fill="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    className="w-5 h-5"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                  </svg>
+                </a>
+                <a className="ml-2 text-gray-500">
+                  <svg
+                    fill="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    className="w-5 h-5"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                  </svg>
+                </a>
+              </span>
             </div>
-            <button className="btn btn-outline btn-secondary btn-wide btn-sm">
-              Price: {product?.price}
-            </button>
-            <p className="pt-6 font-bold">Key Features</p>
+            <p className="leading-relaxed">{product?.description}</p>
+            <p className="pt-2 font-bold">Key Features</p>
             <ul className="space-y-1 text-md list-disc">
               <li>{product?.keyFeatures?.model}</li>
               <li>{product?.keyFeatures?.specification}</li>
             </ul>
-            <p className="pt-6">{product?.description}</p>
-
-            <button className="btn btn-accent">Add To Cart</button>
+            <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5"></div>
+            <div className="flex">
+              <span className="title-font font-medium text-2xl text-gray-900">
+                Price: {product?.price} TK
+              </span>
+              <button className="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">
+                Button
+              </button>
+              <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
+                <svg
+                  fill="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  className="w-5 h-5"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
+                </svg>
+              </button>
+            </div>
+            <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5"></div>
+            <div>
+              <p className="font-bold text-xl text-secondary pb-3">
+                User Reviews:{" "}
+              </p>
+            </div>
+            {product?.reviews.map((review) => (
+              <div key={review.user} className="chat chat-start">
+                <div className="chat-image avatar">
+                  <div className="w-10 rounded-full">
+                    <img
+                      alt="Tailwind CSS chat bubble component"
+                      src="https://i.ibb.co/19nvRVc/photo-1534528741775-53994a69daeb.jpg"
+                    />
+                  </div>
+                </div>
+                <div className="chat-header ">
+                  {review.user}
+                  <time className="text-xs text-primary ps-5">12:45</time>
+                </div>
+                <div className="chat-bubble chat-bubble-secondary">
+                  {review.comment}
+                </div>
+              </div>
+            ))}
+            <div className=""></div>
           </div>
         </div>
       </div>
-      <div className="p-10 w-[60%] mx-auto">fsfsf</div>
-    </>
+    </div>
   );
 };
 
